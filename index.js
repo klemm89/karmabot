9c36f13847105e87a6ddcdeb5055975f18f43a39
avishek.das <avishekd@slalom.com>
2014-11-22 02:08:49 -0600

    removing old express bodyparser

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ var msg = "";
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.use(express.bodyParser());
+app.use(bodyParser.json());
 app.use(app.router);
-app.use(bodyParser.json());       // to support JSON-encoded bodies
-app.use(express.urlencoded()); // to support URL-encoded bodies
+//app.use(express.json());       // to support JSON-encoded bodies
+//app.use(express.urlencoded()); // to support URL-encoded bodies
 
 
 

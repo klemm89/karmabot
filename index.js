ddabc8aa562e481390b0fb3b742a630812c865d1
avishek.das <avishekd@slalom.com>
2014-11-22 01:22:44 -0600

    Adding test update route

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var msg = ""
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+  response.send('Hello World! Msg: ' + msg);
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
+
+/*--------Routes----------*/
+app.post('/update', function(req, res) {
+  msg = req.body.kill;
+  
+  console.log("Message received: " + msg);
+
+  res.send("Updated");
+});
